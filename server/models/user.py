62bfc1976815dde9eca7f870588b980b4ac419bc
Klein <klein.gachuhi@student.moringaschool.com>
2025-06-24 07:25:27 +0300

    User model

---
+++ server/models/user.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from server.config import db, bcrypt
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    @validates("username")
+    def validate_username(self, key, value):
+        if not value or len(value) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        return value
